2a465785623ae16d75e7f5f502788004453f2998
thor <Send.tilmig@gmail.com>
2016-09-13 23:44:48 +0200

    Initial commit2

--- app/src/main/java/com/example/thor/galgraevling/Playscreen.java
+++ app/src/main/java/com/example/thor/galgraevling/Playscreen.java
@@ -1,11 +1,14 @@
 package com.example.thor.galgraevling;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,19 +25,26 @@ public class Playscreen extends AppCompatActivity implements View.OnClickListene
     TextView ordet;
     ImageView hangBilled;
     int billedID[];
+    TextView dødtxt;
+//    Button gætOrd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playscreen);
-          Toast.makeText(this, galgelogik.getOrdet(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, galgelogik.getOrdet(), Toast.LENGTH_LONG).show();
         findViewById(R.id.buttonStartNy).setOnClickListener(this);
+//        gætOrd = (Button) findViewById(R.id.buttonGæt);
+//        gætOrd.setOnClickListener(this);
         brugerInput = (EditText) findViewById(R.id.editTextBrugerInput);
         brugtBogstaver = (TextView) findViewById(R.id.textViewBrugt);
         ordet = (TextView) findViewById(R.id.textViewGetteOrd);
         hangBilled = (ImageView) findViewById(R.id.imageView);
+        dødtxt = (TextView) findViewById(R.id.textViewDød);
         brugerInput.setOnKeyListener(this);
         brugerInput.setOnClickListener(this);
+//        gætOrd.setActivated(false);
+
 
         billedID = new int[R.drawable.class.getFields().length];
         billedID[0] = R.drawable.galge;
@@ -71,6 +81,12 @@ public class Playscreen extends AppCompatActivity implements View.OnClickListene
         switch (v.getId()) {
             case R.id.buttonStartNy:
                 galgelogik.nulstil();
+                dødtxt.setText("");
+                brugtBogstaver.setText(galgelogik.getBrugteBogstaver().toString());
+                ordet.setText(galgelogik.getSynligtOrd());
+                hangBilled.setImageResource(billedID[galgelogik.getAntalForkerteBogstaver()]);
+
+//                gætOrd.setActivated(true);
                 break;
 
             case R.id.editTextBrugerInput:
@@ -85,19 +101,32 @@ public class Playscreen extends AppCompatActivity implements View.OnClickListene
 
     }
 
-    int foo = 0;
 
     @Override
     public boolean onKey(View v, int keyCode, KeyEvent event) {
-        foo++;
         if (event.getAction() == KeyEvent.ACTION_UP) {
-            System.out.println("" + foo);
+            if (galgelogik.erSpilletSlut()) return false;
             galgelogik.gætBogstav(brugerInput.getText().toString());
-            if(galgelogik.erSidsteBogstavKorrekt()){
+            if (galgelogik.erSidsteBogstavKorrekt()) {
                 ordet.setText(galgelogik.getSynligtOrd());
-            }else {
-                brugtBogstaver.setText(galgelogik.getBrugteBogstaver().toString());
-                hangBilled.setImageResource(billedID[galgelogik.getAntalForkerteBogstaver()%billedID.length]);
+                if (galgelogik.erSpilletVundet()){
+                    dødtxt.setText("TILLYKKE DU VANDT");
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+//                gætOrd.setActivated(true);
+
+            } else {
+
+                hangBilled.setImageResource(billedID[galgelogik.getAntalForkerteBogstaver()]);
+                System.out.println(galgelogik.getAntalForkerteBogstaver());
+
+                if (galgelogik.getAntalForkerteBogstaver() > 5) {
+                    dødtxt.setText("DU DUM OG DØD");
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+//                    gætOrd.setActivated(false);
+                }
             }
 
 //            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -105,6 +134,7 @@ public class Playscreen extends AppCompatActivity implements View.OnClickListene
 
             // Toast.makeText(this, brugerInput.getText(), Toast.LENGTH_SHORT).show();
         }
+        brugtBogstaver.setText(galgelogik.getBrugteBogstaver().toString());
 
         return false;
     }
